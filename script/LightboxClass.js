98588744bf5fee8108fc390ad0cb48a91fd9ca84
Nicolas Chatelais <ni.chatelais@gmail.com>
2021-03-15 10:51:41 +0100

    fix problem img display

--- script/LightboxClass.js
+++ script/LightboxClass.js
@@ -1,5 +1,4 @@
 
-
 /*** DECLARATIONS ***/
 
 const lightbox = document.getElementById("displayLightbox");
@@ -8,7 +7,7 @@ const lightboxModal = document.getElementById("lightbox-modal");
 const nextImg = document.getElementById("nextIcon");
 const prevImg = document.getElementById("prevIcon");
 const body = document.body;
-   
+const closeIcon = document.getElementById("closeIcon"); 
 
 
 /*** OBJET QUI GERE L'AFFICHAGE DES MEDIAS EN PLEIN ECRAN (carrousel) ***/
@@ -33,7 +32,7 @@ export class Lightbox{
 
         this.generateLightbox = function(index){
             lightbox.style.display = "flex";
-            body.style.overflowY = "hidden";
+            displayVerticalScrollBar(false)
             if(this.medias[index].nodeName == "IMG"){
                 lightboxModal.innerHTML = `<img src="${this.medias[index].src}" alt="${this.medias[index].alt}" class="lightbox-modal__box--photo" style="max-width:100%"></img>`
                 photoTitle.innerHTML = `${this.medias[index].alt}`
@@ -45,60 +44,39 @@ export class Lightbox{
         },
         
 
-/*Fonction d'écoute pour affichage de la lightbox appelé après chaque tri*/
+/*Méthode d'écoute pour affichage de la lightbox appelé après chaque tri*/
 
         this.navigationInLightbox = function(){
             
-            let mediaIndex = 0;
-            
             let medias = Array.from(document.querySelectorAll(".media"))
-            
             let lightboxContent = new Lightbox(medias)
-            
             let maxMediaIndex = medias.length
+            let mediaIndex = 0;
 
-            medias.forEach(media => {
-                    
-                media.addEventListener("click", ()=>{
-                    
+            medias.forEach(media => {        
+                media.addEventListener("click", ()=>{ 
                     mediaIndex = media.index
                     lightboxContent.generateLightbox(mediaIndex)
-                    
-                    //console.log(count)
-                    
                 })
 
-                media.addEventListener("keydown", (e)=>{
-                
-                    //count = media.index
-                    
+                media.addEventListener("keydown", (e)=>{                   
                     switch(e.key){
                         case 'Enter':
-                            console.log('entrée')
                             mediaIndex = media.index     
                             this.generateLightbox(mediaIndex)
                             break;  
                         case 'Escape':
-                            console.log('échap')
-                            document.getElementById("displayLightbox").style.display = "none";
-                            body.style.overflowY = "visible"
-                            //media.removeEventListener("keydown",(e))
+                            closeLightbox(true)
                             break;
-                            
                         case 'ArrowLeft':
-                            console.log('arrière')
                             mediaIndex--
                             mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
                             this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
                             return mediaIndex
-                            
                         case 'ArrowRight':
-                            console.log('avant')
                             mediaIndex++
                             mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
                             this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
                             return mediaIndex
                         default:
                             console.log(e)
@@ -111,157 +89,82 @@ export class Lightbox{
                 mediaIndex++
                 mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
                 this.generateLightbox(mediaIndex)
-                console.log(mediaIndex)
-                //console.log(this)
-                console.log(this.isAlreadyListen)
-                
                 
                 if (!this.isAlreadyListen){
                     lightbox.addEventListener('keydown', (e)=>{
                     switch(e.key){
                         case 'Escape':
-                            console.log('échap')
-                            document.getElementById("displayLightbox").style.display = "none";
-                            body.style.overflowY = "visible"
-                            //media.removeEventListener("keydown",(e))
-                            break;
-                            
+                            closeLightbox(true)
+                            break;   
                         case 'ArrowLeft':
-                            console.log('arrière')
                             mediaIndex--
                             mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
                             this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
                             return mediaIndex
-                            
                         case 'ArrowRight':
-                            console.log('avant')
                             mediaIndex++
                             mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
                             this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
                             return mediaIndex
-
-                        
                         default:
                             console.log(e)
                     }
                 })
                 this.isAlreadyListen = true;
                 }
-                
-               /*
-                lightbox.addEventListener('keydown', (e)=>{
-                    switch(e.key){
-                        case 'Escape':
-                            console.log('échap')
-                            document.getElementById("displayLightbox").style.display = "none";
-                            body.style.overflowY = "visible"
-                            //media.removeEventListener("keydown",(e))
-                            break;
-                            
-                        case 'ArrowLeft':
-                            console.log('arrière')
-                            mediaIndex--
-                            mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
-                            this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
-                            return mediaIndex
-                            
-                        case 'ArrowRight':
-                            console.log('avant')
-                            mediaIndex++
-                            mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
-                            this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
-                            return mediaIndex
-
-                        
-                        default:
-                            console.log(e)
-                    }
-                })*/
             })
+
             prevImg.addEventListener("click", ()=>{
                 mediaIndex--
                 mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
                 this.generateLightbox(mediaIndex)
-                console.log(mediaIndex)
-                lightbox.addEventListener('keydown', (e)=>{
-                    switch(e.key){
-                        case 'Escape':
-                            console.log('échap')
-                            document.getElementById("displayLightbox").style.display = "none";
-                            body.style.overflowY = "visible"
-                            //media.removeEventListener("keydown",(e))
-                            break;
-                            
-                        case 'ArrowLeft':
-                            console.log('arrière')
-                            mediaIndex--
-                            mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
-                            this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
-                            return mediaIndex
-                            
-                        case 'ArrowRight':
-                            console.log('avant')
-                            mediaIndex++
-                            mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
-                            this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
-                            return mediaIndex
-
-                        
-                        default:
-                            console.log(e)
-                    }
-                })
-            })
-            /*
-            medias.forEach(media => {
-                     /*    
-                        case 'Escape':
-                            console.log('échap')
-                            document.getElementById("displayLightbox").style.display = "none";
-                            body.style.overflowY = "visible"
-                            media.removeEventListener("keydown",(e))
-                            break;
-                            
-                        case 'ArrowLeft':
-                            console.log('arrière')
-                            mediaIndex--
-                            mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
-                            this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
-                            return mediaIndex
-                            
-                        case 'ArrowRight':
-                            console.log('avant')
-                            mediaIndex++
-                            mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
-                            this.generateLightbox(mediaIndex)
-                            console.log(mediaIndex)
-                            return mediaIndex
-
-            })*/
-            //console.log(medias)
+                
+                if (!this.isAlreadyListen){
+                    lightbox.addEventListener('keydown', (e)=>{
+                        switch(e.key){
+                            case 'Escape':
+                                closeLightbox(true)
+                                break;
+                            case 'ArrowLeft':
+                                mediaIndex--
+                                mediaIndex < 0 ? mediaIndex = maxMediaIndex-1 : mediaIndex
+                                this.generateLightbox(mediaIndex)
+                                return mediaIndex
+                            case 'ArrowRight':
+                                mediaIndex++
+                                mediaIndex > maxMediaIndex-1 ? mediaIndex = 0 : mediaIndex
+                                this.generateLightbox(mediaIndex)
+                                return mediaIndex
+                            default:
+                                console.log(e)
+                        }
+                    })
+                this.isAlreadyListen = true;
+                }
+            }) 
         }
-        
     }
 }
 
 
+
 //Fermeture de la lightbox par icône croix
 
-const closeIcon = document.getElementById("closeIcon");
-/*
-lightbox.addEventListener('click', ()=>{
-    console.log(document.getElementById('lightbox-modal').firstChild)
-})
-*/
 closeIcon.addEventListener("click", ()=>{
-    document.getElementById("displayLightbox").style.display = "none";
-    body.style.overflowY = "visible"
+    lightbox.style.display = "none";
+    displayVerticalScrollBar(true)
 });
+
+function closeLightbox(bool){
+    displayVerticalScrollBar(bool)
+    lightbox.style.display = "none";
+}
+
+function displayVerticalScrollBar(bool){
+    if(bool){
+        body.style.overflowY = "visible"
+    }else{
+        body.style.overflowY = "hidden"
+    }
+}
 
